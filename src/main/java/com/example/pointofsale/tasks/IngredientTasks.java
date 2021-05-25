9eea4c953a08dce3f8c6d8906a0ad468b4d48811
dakotac5 <chasecrossley99@gmail.com>
2021-05-24 21:47:18 -0700

    updated Ingredient Tasks

--- src/main/java/com/example/pointofsale/tasks/IngredientTasks.java
+++ src/main/java/com/example/pointofsale/tasks/IngredientTasks.java
@@ -1,13 +1,9 @@
 package com.example.pointofsale.tasks;
 
-import com.example.pointofsale.daos.ServingUnitDao;
 import com.example.pointofsale.entities.Ingredient;
-import com.example.pointofsale.entities.ServingUnit;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 
 @Component
@@ -20,9 +16,10 @@ public class IngredientTasks extends BaseTasks<Ingredient> {
     }
 
     @Override
-    Ingredient getNewEntity() {
-        final List<ServingUnit> servingUnits = (List<ServingUnit>) servingUnitDao.allObjects();
-        ServingUnit servingUnit = servingUnits.get(RandomUtils.nextInt(0, servingUnits.size() - 1));
-        return new Ingredient(RandomUtils.nextInt(1, 10), RandomUtils.nextInt(), servingUnit);
+    public Ingredient getNewEntity() {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName(RandomStringUtils.randomAlphabetic(RandomUtils.nextInt(1, NAME_MAX_SIZE)));
+        return ingredient;
     }
 }
+
